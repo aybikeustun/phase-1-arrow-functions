e0cc653e877ad3c6f127b3f6b14d0350dd9da2d0
aybikeustun <aybikeustun@gmail.com>
2023-04-21 18:59:48 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+  
+  console.log(divide(2000, 100));
+
+  const square = x => x*x
+
+  const add = (parameter1, parameter2) => parameter1 + parameter2;
